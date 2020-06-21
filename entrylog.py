d0fe01af29a50c390c362e6f69cac7a22e8155dc
Aiman Faruqy <aimanfaruqy1@gmail.com>
2020-06-21 17:40:37 +0800

    Center department option window

--- entrylog.py
+++ entrylog.py
@@ -279,12 +279,24 @@ class Gui(threading.Thread):
 root = tk.Tk()
 root.title("Uniten TAS v3.0 Department")
 
+# Gets the requested values of the height and widht.
+windowWidth = root.winfo_reqwidth()
+windowHeight = root.winfo_reqheight()
+ 
+# Gets both half the screen width/height and window width/height
+positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
+positionDown = int(root.winfo_screenheight()/2 - windowHeight/2)
+ 
+# Positions the window in the center of the page.
+root.geometry("+{}+{}".format(positionRight, positionDown))
+
 # Add a grid
 mainframe = Frame(root)
 mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
 mainframe.columnconfigure(0, weight = 1)
 mainframe.rowconfigure(0, weight = 1)
 mainframe.pack(pady = 100, padx = 100)
+
 
 # Create a Tkinter variable
 tkvar = StringVar(root)
--- exitlog.py
+++ exitlog.py
@@ -301,6 +301,17 @@ class Gui(threading.Thread):
 root = tk.Tk()
 root.title("Uniten TAS v3.0 Department")
 
+# Gets the requested values of the height and widht.
+windowWidth = root.winfo_reqwidth()
+windowHeight = root.winfo_reqheight()
+ 
+# Gets both half the screen width/height and window width/height
+positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
+positionDown = int(root.winfo_screenheight()/2 - windowHeight/2)
+ 
+# Positions the window in the center of the page.
+root.geometry("+{}+{}".format(positionRight, positionDown))
+
 # Add a grid
 mainframe = Frame(root)
 mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
